ba58deee9506d583f6daf17e9a7bb9080d811282
zloypur <zloypur@gmail.com>
2017-10-25 12:28:24 +0300

    new Lab

---
+++ Lab2/src/main/java/ru/spbstu/telematics/java/Lab2.java
@@ -0,0 +1,13 @@
+package ru.spbstu.telematics.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class Lab2
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
