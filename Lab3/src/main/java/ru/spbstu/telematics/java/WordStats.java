4f373573c4325a1f15423274f35e6d5d4e205ab6
zloypur <zloypur@gmail.com>
2017-12-19 19:29:34 +0300

    Lab & Test

---
+++ Lab3/src/main/java/ru/spbstu/telematics/java/WordStats.java
@@ -0,0 +1,78 @@
+package ru.spbstu.telematics.java;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.TreeMap;
+
+
+public class WordStats {
+
+    private Map<String, Integer> statisic;
+
+    WordStats(String[] files) throws InterruptedException{
+        statisic = new TreeMap<String, Integer>();
+
+        Thread[] tr = new Thread[files.length];
+
+        for(int i = 0; i < files.length; i++){
+            FileStats f = new FileStats(files[i]);
+            tr[i] = new Thread(f);
+            tr[i].start();
+            try {
+                tr[i].join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                tr[i].interrupt();
+            }
+        }
+    }
+
+    public Map<String, Integer> getStatisic() {
+        return statisic;
+    }
+
+    private class FileStats implements Runnable {
+
+        private String text;
+
+        public FileStats(String text){
+            this.text = text;
+        }
+
+
+        @Override
+        public void run() {
+            String[] words = text.split("[\\W]+");//this regexp works only with english words
+
+            //for(String s : words)
+              //  System.out.println(s);
+
+            Map<String, Integer> tmpStat = new TreeMap<>();
+
+
+            for(String w : words)
+                if(!Thread.currentThread().isInterrupted()) {
+                    if (tmpStat.containsKey(w))
+                        tmpStat.replace(w, tmpStat.get(w) + 1);
+                    else
+                        tmpStat.put(w, 1);
+                }else {
+                    System.out.println("Thread is interrupted");
+                    return;
+                }
+
+            for(Map.Entry<String, Integer> entry : tmpStat.entrySet())
+                if(!Thread.currentThread().isInterrupted()) {
+                    synchronized (statisic) {
+                        if (statisic.containsKey(entry.getKey()))
+                            statisic.replace(entry.getKey(), statisic.get(entry.getKey()) + entry.getValue());
+                        else
+                            statisic.put(entry.getKey(), entry.getValue());
+                    }
+                }else {
+                    System.out.println("Thread is interrupted");
+                    return;
+                }
+        }
+    }
+}
